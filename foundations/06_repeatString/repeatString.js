85d000eae480f07e4e28284b33e2b9235366da35
Parth-Verma-007 <greatparth007@gmail.com>
2026-03-09 22:43:02 +0530

    Completed 06_repeatString of foundation in javascript-exercises

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+function loop(str,num){
+    if (num < 0){
+        return "ERROR";
+    }
+    let i;
+    let string = "";
+    for (i = 0; i<num; i++){
+        string += str;
+    }
+    return string;
+}
+const repeatString = loop("HeheHaha",2);
+console.log(repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
