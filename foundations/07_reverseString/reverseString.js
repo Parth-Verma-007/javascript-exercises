9489e025d695d40ad1080dba32d186448008557e
Parth-Verma-007 <greatparth007@gmail.com>
2026-03-10 22:13:05 +0530

    Completed 07_reverseString of foundation in javascript-exercises

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+function reverseString(string){
+    let rev="";
+    for(let i = string.length - 1; i >= 0; i--){
+        rev += string[i];
+    }
+    return rev;
 };
 
+const reversal = reverseString("Hello, My name is Parth Verma.");
+
+console.log(reversal);
 // Do not edit below this line
 module.exports = reverseString;
