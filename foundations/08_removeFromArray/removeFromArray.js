12281286697f03283bd54a7e5415691929201adb
Parth-Verma-007 <greatparth007@gmail.com>
2026-03-20 22:28:45 +0530

    Completed 08_removeFromArray of foundation in javascript-exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
-
+const arr = [1,2,3,4,5,6];
+function removeFromArray(array,...num){
+    const newArr = [];
+    for(let i =0; i < array.length; i++){
+        if(!num.includes(array[i])){
+            newArr.push(array[i]);
+        }
+    }
+    return newArr;
+}
+const newArr = removeFromArray(arr,3,4,5);
+console.log(newArr);
 // Do not edit below this line
 module.exports = removeFromArray;
