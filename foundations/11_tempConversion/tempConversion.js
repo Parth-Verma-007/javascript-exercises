8d2d3f8f72c6290f010a9fac0c815cce109de79c
Parth-Verma-007 <greatparth007@gmail.com>
2026-03-21 12:13:14 +0530

    completed 11_tempConversion of foundation in javascript-exercises

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+function convertToCelsius(F) {
+  const celsius = (5/9)*(F-32);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(C) {
+  const Fahhh = (C * (9/5)) + 32;
+  return Math.round(Fahhh * 10) / 10;
+}; 
 
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
