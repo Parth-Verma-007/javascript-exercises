b0f1a6bb13524b23f315ca34444aea01a41f1230
Parth-Verma-007 <greatparth007@gmail.com>
2026-03-21 11:28:13 +0530

    Completed 09_SumAll of foundation in javascript-exercises

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
-};
-
+function sumAll(x,y){
+    let sum = 0;
+    for(let i = x; i <= y; i++){
+        sum = sum + i
+    }
+    return sum;
+}
+console.log(sumAll(1,2));
 // Do not edit below this line
 module.exports = sumAll;
