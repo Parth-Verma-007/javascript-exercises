5c086938eeca7e020f7b9aa330574c9542c32bad
Parth-Verma-007 <greatparth007@gmail.com>
2026-03-21 11:39:11 +0530

    Completed 10_leapYears of foundation in javascript-exercises

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+function leapYears(year){
+    if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
 
-};
+const isLeap = leapYears(1800);
+
+console.log(isLeap);
 
 // Do not edit below this line
 module.exports = leapYears;
